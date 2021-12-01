bb7ee08bb5c608eb43aaf5be1f5aa65ef6fb35bc
Pavitra biradar <pavitrabiradar25@gmail.com>
2021-12-01 10:11:26 +0530

    first commit

---
+++ bluebits/apps/admin/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [AppComponent, DashboardComponent],
+  imports: [BrowserModule,RouterModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
